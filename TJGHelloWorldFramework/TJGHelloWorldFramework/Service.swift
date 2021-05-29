653707a366b360b3d2829c4a52a988c2e5e3c6e6
Gourav Jamwal <gjamwal@vmware.com>
2021-05-29 20:40:34 +0530

    Finished our hello world framework

---
+++ TJGHelloWorldFramework/TJGHelloWorldFramework/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  TJGHelloWorldFramework
+//
+//  Created by Gourav Jamwal on 29/05/21.
+//
+
+import Foundation
+
+public class Service {
+
+    private init() {}
+
+    public static func doSomething() -> String {
+        return "My work here just started. Hello World!"
+    }
+}
---
+++ TJGHelloWorldFramework/TJGHelloWorldFramework/TJGHelloWorldFramework.h
@@ -0,0 +1,18 @@
+//
+//  TJGHelloWorldFramework.h
+//  TJGHelloWorldFramework
+//
+//  Created by Gourav Jamwal on 29/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TJGHelloWorldFramework.
+FOUNDATION_EXPORT double TJGHelloWorldFrameworkVersionNumber;
+
+//! Project version string for TJGHelloWorldFramework.
+FOUNDATION_EXPORT const unsigned char TJGHelloWorldFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TJGHelloWorldFramework/PublicHeader.h>
+
+
